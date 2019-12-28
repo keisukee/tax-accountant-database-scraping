3980ed11267f424a9a8f07e944a0a96158c385bf
keisuke ohashi <kog71903@gmail.com>
2019-12-28 11:37:02 +0900

    [update]スクレイピング完了

--- get_all_data.sh
+++ get_all_data.sh
@@ -2,36 +2,40 @@
 
 # ruby scrape_and_output.rb input/yokohama_data.txt > output/yokohama_output.txt
 # ruby scrape_and_output.rb input/kyoto_data.txt > output/kyoto_output.txt
-ruby scrape_and_output.rb input/saitama_data.txt > output/saitama_output.txt
-ruby scrape_and_output.rb input/kawaguchi_data.txt > output/kawaguchi_output.txt
-ruby scrape_and_output.rb input/kawagoe_data.txt > output/kawagoe_output.txt
-ruby scrape_and_output.rb input/kawasaki_data.txt > output/kawasaki_output.txt
-ruby scrape_and_output.rb input/sagamihara_data.txt > output/sagamihara_output.txt
-ruby scrape_and_output.rb input/fujisawa_data.txt > output/fujisawa_output.txt
-ruby scrape_and_output.rb input/chigasaki_data.txt > output/chigasaki_output.txt
+# ruby scrape_and_output.rb input/saitama_data.txt > output/saitama_output.txt
+# ruby scrape_and_output.rb input/kawaguchi_data.txt > output/kawaguchi_output.txt
+# ruby scrape_and_output.rb input/kawagoe_data.txt > output/kawagoe_output.txt
+# ruby scrape_and_output.rb input/kawasaki_data.txt > output/kawasaki_output.txt
+# ruby scrape_and_output.rb input/sagamihara_data.txt > output/sagamihara_output.txt
+# ruby scrape_and_output.rb input/fujisawa_data.txt > output/fujisawa_output.txt
+# ruby scrape_and_output.rb input/chigasaki_data.txt > output/chigasaki_output.txt
 ruby scrape_and_output.rb input/chiba_data.txt > output/chiba_output.txt
 
 ruby scrape_and_output.rb input/funabashi_data.txt > output/funabashi_output.txt
-ruby scrape_and_output.rb input/ichikawa_data.txt > output/ichikawa_output.txt
-ruby scrape_and_output.rb input/kashiwa_data.txt > output/kashiwa_output.txt
 
-ruby scrape_and_output.rb input/matsudo_data.txt > output/matsudo_output.txt
-ruby scrape_and_output.rb input/osaka_data.txt > output/osaka_output.txt
-ruby scrape_and_output.rb input/maikata_data.txt > output/maikata_output.txt
+# ruby scrape_and_output.rb input/ichikawa_data.txt > output/ichikawa_output.txt
+# ruby scrape_and_output.rb input/kashiwa_data.txt > output/kashiwa_output.txt
+
+# ruby scrape_and_output.rb input/matsudo_data.txt > output/matsudo_output.txt
+# ruby scrape_and_output.rb input/osaka_data.txt > output/osaka_output.txt
+# ruby scrape_and_output.rb input/maikata_data.txt > output/maikata_output.txt
+
 ruby scrape_and_output.rb input/sakai_data.txt > output/sakai_output.txt
-ruby scrape_and_output.rb input/setagaya_data.txt > output/setagaya_output.txt
-ruby scrape_and_output.rb input/suginami_data.txt > output/suginami_output.txt
-ruby scrape_and_output.rb input/minato_data.txt > output/minato_output.txt
-ruby scrape_and_output.rb input/meguro_data.txt > output/meguro_output.txt
+
+# ruby scrape_and_output.rb input/setagaya_data.txt > output/setagaya_output.txt
+# ruby scrape_and_output.rb input/suginami_data.txt > output/suginami_output.txt
+# ruby scrape_and_output.rb input/minato_data.txt > output/minato_output.txt
+# ruby scrape_and_output.rb input/meguro_data.txt > output/meguro_output.txt
 ruby scrape_and_output.rb input/chuo_data.txt > output/chuo_output.txt
-ruby scrape_and_output.rb input/nakano_data.txt > output/nakano_output.txt
-ruby scrape_and_output.rb input/edogawa_data.txt > output/edogawa_output.txt
-ruby scrape_and_output.rb input/adachi_data.txt > output/adachi_output.txt
-ruby scrape_and_output.rb input/itabashi_data.txt > output/itabashi_output.txt
-ruby scrape_and_output.rb input/katsushika_data.txt > output/katsushika_output.txt
-ruby scrape_and_output.rb input/toshima_data.txt > output/toshima_output.txt
-ruby scrape_and_output.rb input/machida_data.txt > output/machida_output.txt
-ruby scrape_and_output.rb input/sumida_data.txt > output/sumida_output.txt
-ruby scrape_and_output.rb input/chiyoda_data.txt > output/chiyoda_output.txt
-ruby scrape_and_output.rb input/kita_data.txt > output/kita_output.txt
-ruby scrape_and_output.rb input/kobe_data.txt > output/kobe_output.txt
+
+# ruby scrape_and_output.rb input/nakano_data.txt > output/nakano_output.txt
+# ruby scrape_and_output.rb input/edogawa_data.txt > output/edogawa_output.txt
+# ruby scrape_and_output.rb input/adachi_data.txt > output/adachi_output.txt
+# ruby scrape_and_output.rb input/itabashi_data.txt > output/itabashi_output.txt
+# ruby scrape_and_output.rb input/katsushika_data.txt > output/katsushika_output.txt
+# ruby scrape_and_output.rb input/toshima_data.txt > output/toshima_output.txt
+# ruby scrape_and_output.rb input/machida_data.txt > output/machida_output.txt
+# ruby scrape_and_output.rb input/sumida_data.txt > output/sumida_output.txt
+# ruby scrape_and_output.rb input/chiyoda_data.txt > output/chiyoda_output.txt
+# ruby scrape_and_output.rb input/kita_data.txt > output/kita_output.txt
+# ruby scrape_and_output.rb input/kobe_data.txt > output/kobe_output.txt

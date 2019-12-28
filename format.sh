4812b79bad64a85c6116959de27d96808fe28fd3
keisuke ohashi <kog71903@gmail.com>
2019-12-28 12:03:06 +0900

    [update]エクセル出力完了

--- format.sh
+++ format.sh
@@ -1,35 +1,68 @@
 #!/bin/zsh
 
 ruby correct_format_and_output.rb output/yokohama_output.txt formatted/yokohama_output.xls
+echo "yokohama_output.txt formatted/yokohama_output.xls"
 ruby correct_format_and_output.rb output/kyoto_output.txt formatted/kyoto_output.xls
+echo "kyoto_output.txt formatted/kyoto_output.xls"
 ruby correct_format_and_output.rb output/saitama_output.txt formatted/saitama_output.xls
+echo "saitama_output.txt formatted/saitama_output.xls"
 ruby correct_format_and_output.rb output/kawaguchi_output.txt formatted/kawaguchi_output.xls
+echo "kawaguchi_output.txt formatted/kawaguchi_output.xls"
 ruby correct_format_and_output.rb output/kawagoe_output.txt formatted/kawagoe_output.xls
+echo "kawagoe_output.txt formatted/kawagoe_output.xls"
 ruby correct_format_and_output.rb output/kawasaki_output.txt formatted/kawasaki_output.xls
+echo "kawasaki_output.txt formatted/kawasaki_output.xls"
 ruby correct_format_and_output.rb output/sagamihara_output.txt formatted/sagamihara_output.xls
+echo "sagamihara_output.txt formatted/sagamihara_output.xls"
 ruby correct_format_and_output.rb output/fujisawa_output.txt formatted/fujisawa_output.xls
+echo "fujisawa_output.txt formatted/fujisawa_output.xls"
 ruby correct_format_and_output.rb output/chigasaki_output.txt formatted/chigasaki_output.xls
+echo "chigasaki_output.txt formatted/chigasaki_output.xls"
 ruby correct_format_and_output.rb output/chiba_output.txt formatted/chiba_output.xls
+echo "chiba_output.txt formatted/chiba_output.xls"
 ruby correct_format_and_output.rb output/funabashi_output.txt formatted/funabashi_output.xls
+echo "funabashi_output.txt formatted/funabashi_output.xls"
 ruby correct_format_and_output.rb output/ichikawa_output.txt formatted/ichikawa_output.xls
+echo "ichikawa_output.txt formatted/ichikawa_output.xls"
 ruby correct_format_and_output.rb output/kashiwa_output.txt formatted/kashiwa_output.xls
+echo "kashiwa_output.txt formatted/kashiwa_output.xls"
 ruby correct_format_and_output.rb output/matsudo_output.txt formatted/matsudo_output.xls
+echo "matsudo_output.txt formatted/matsudo_output.xls"
 ruby correct_format_and_output.rb output/osaka_output.txt formatted/osaka_output.xls
+echo "osaka_output.txt formatted/osaka_output.xls"
 ruby correct_format_and_output.rb output/maikata_output.txt formatted/maikata_output.xls
+echo "maikata_output.txt formatted/maikata_output.xls"
 ruby correct_format_and_output.rb output/sakai_output.txt formatted/sakai_output.xls
+echo "sakai_output.txt formatted/sakai_output.xls"
 ruby correct_format_and_output.rb output/setagaya_output.txt formatted/setagaya_output.xls
+echo "setagaya_output.txt formatted/setagaya_output.xls"
 ruby correct_format_and_output.rb output/suginami_output.txt formatted/suginami_output.xls
+echo "suginami_output.txt formatted/suginami_output.xls"
 ruby correct_format_and_output.rb output/minato_output.txt formatted/minato_output.xls
+echo "minato_output.txt formatted/minato_output.xls"
 ruby correct_format_and_output.rb output/meguro_output.txt formatted/meguro_output.xls
+echo "meguro_output.txt formatted/meguro_output.xls"
 ruby correct_format_and_output.rb output/chuo_output.txt formatted/chuo_output.xls
+echo "chuo_output.txt formatted/chuo_output.xls"
 ruby correct_format_and_output.rb output/nakano_output.txt formatted/nakano_output.xls
+echo "nakano_output.txt formatted/nakano_output.xls"
 ruby correct_format_and_output.rb output/edogawa_output.txt formatted/edogawa_output.xls
+echo "edogawa_output.txt formatted/edogawa_output.xls"
 ruby correct_format_and_output.rb output/adachi_output.txt formatted/adachi_output.xls
+echo "adachi_output.txt formatted/adachi_output.xls"
 ruby correct_format_and_output.rb output/itabashi_output.txt formatted/itabashi_output.xls
+echo "itabashi_output.txt formatted/itabashi_output.xls"
 ruby correct_format_and_output.rb output/katsushika_output.txt formatted/katsushika_output.xls
+echo "katsushika_output.txt formatted/katsushika_output.xls"
 ruby correct_format_and_output.rb output/toshima_output.txt formatted/toshima_output.xls
+echo "toshima_output.txt formatted/toshima_output.xls"
 ruby correct_format_and_output.rb output/machida_output.txt formatted/machida_output.xls
+echo "machida_output.txt formatted/machida_output.xls"
 ruby correct_format_and_output.rb output/sumida_output.txt formatted/sumida_output.xls
+echo "sumida_output.txt formatted/sumida_output.xls"
 ruby correct_format_and_output.rb output/chiyoda_output.txt formatted/chiyoda_output.xls
+echo "chiyoda_output.txt formatted/chiyoda_output.xls"
 ruby correct_format_and_output.rb output/kita_output.txt formatted/kita_output.xls
+echo "kita_output.txt formatted/kita_output.xls"
 ruby correct_format_and_output.rb output/kobe_output.txt formatted/kobe_output.xls
+echo "kobe_output.txt formatted/kobe_output.xls"
